b5821c1979c06cbfb2966dcb1a4acab22962cc1c
Alan Ng <alan.ng@blake.com.au>
2019-06-19 04:37:26 +1000

    done contact mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,16 @@ module.exports = {
         description: 
             "A site we built together during a full-dsay frontend masters gatsby workshop",
     },
-    plugins: ['gatsby-plugin-emotion'],
+    plugins: [
+        'gatsby-plugin-emotion',
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-mdx',
+            options: {
+                defaultLayouts: {
+                    default: require.resolve('./src/components/layout.js'),
+                }
+            }
+        }
+    ],
 }
