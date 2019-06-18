7052347f35198c20aa5b2d829fc9e5da304ba4a7
Alan Ng <alan.ng@blake.com.au>
2019-06-19 04:08:07 +1000

    added header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
+import Header from './header';
 
 const Layout = ({ children }) => (
     <>
@@ -48,7 +49,7 @@ const Layout = ({ children }) => (
             }
         `} 
         />
-        <header></header>
+        <Header />
         <main
             css={css`
                 margin: 2rem auto 4rem;
