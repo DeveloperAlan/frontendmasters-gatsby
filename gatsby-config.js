1bfd543ca69eea918f2ce4078e3db34e192eba06
Alan Ng <alan.ng@blake.com.au>
2019-06-20 04:17:50 +1000

    added gatsby remark images and made image work through markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,8 @@ module.exports = {
             options: {
                 defaultLayouts: {
                     default: require.resolve('./src/components/layout.js'),
-                }
+                },
+                gatsbyRemarkPlugins: [{ resolve: 'gatsby-remark-images'}],
             }
         },
         {
