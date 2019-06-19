bf1ff6889d039bc03af6c599120061e152b7599f
Alan Ng <alan.ng@blake.com.au>
2019-06-20 04:25:34 +1000

    created instagram component

---
+++ src/components/insta.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Image from 'gatsby-image';
+import { css } from '@emotion/core';
+
+const Insta = () => {
+    return (
+        <>
+            <h2>Instagram posts from @dev_alan</h2>
+            <div></div>
+            <a href={'https://instagram.com/dev_alan'}>
+                See more on instagram &rarr;
+            </a>
+        </>
+    )
+}
+
+export default Insta;
